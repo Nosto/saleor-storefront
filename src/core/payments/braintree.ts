37840889fe89e67d33d97e45c96c30afe5c359d2
Mridang Agarwalla <mridang@nosto.com>
2019-02-24 12:49:27 +0200

    Undid the braintree tweaks

--- src/core/payments/braintree.ts
+++ src/core/payments/braintree.ts
@@ -16,8 +16,34 @@ export interface ErrorData {
 
 export const braintreePayment = (paymentClientToken: string, creditCard: any) =>
   new Promise<PaymentData | ErrorData[]>((resolve, reject) => {
-    const lastDigits = "4444";
-    const ccType = "JCB";
-    const token = "245255";
-    resolve({ lastDigits, ccType, token });
+    braintree.client.create(
+      {
+        authorization: paymentClientToken
+      },
+      (err, client) => {
+        client.request(
+          {
+            data: { creditCard },
+            endpoint: "payment_methods/credit_cards",
+            method: "post"
+          },
+          (error, response) => {
+            if (error) {
+              if (error.details.originalError.fieldErrors.length > 0) {
+                error.details.originalError.fieldErrors.map(error => {
+                  if (error.field === "creditCard") {
+                    reject(error.fieldErrors);
+                  }
+                });
+              }
+            } else {
+              const lastDigits = response.creditCards[0].details.lastFour;
+              const ccType = response.creditCards[0].details.cardType;
+              const token = response.creditCards[0].nonce;
+              resolve({ lastDigits, ccType, token });
+            }
+          }
+        );
+      }
+    );
   });
